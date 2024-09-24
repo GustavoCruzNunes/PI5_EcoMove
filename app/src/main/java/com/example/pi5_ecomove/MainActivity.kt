d7f3f0f134a4d55bab0a74b3190872b7ddb45953
Gustavo Nunes <Gustavocnunes13@gmail.com>
2024-09-24 14:05:36 -0300

    Tela De Login

--- app/src/main/java/com/example/pi5_ecomove/MainActivity.kt
+++ app/src/main/java/com/example/pi5_ecomove/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.pi5_ecomove
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+
+        btnLogin.setOnClickListener {
+            // Implementar a ação de login
+            Snackbar.make(it, "Login clicado!", Snackbar.LENGTH_SHORT).show()
+        }
+
+        btnRegister.setOnClickListener {
+            // Implementar a ação de registro
+            Snackbar.make(it, "Cadastrar clicado!", Snackbar.LENGTH_SHORT).show()
         }
     }
 }
